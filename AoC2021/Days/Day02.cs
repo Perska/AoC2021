c1abf3c9bb4e21a0841f44a54ab5f9c71763cd56
Perska <perska.weegee@gmail.com>
2021-12-02 07:20:14 +0200

    Add day 2, enable underflow/overflow checks just in case

--- AoC2021/Days/Day02.cs
+++ AoC2021/Days/Day02.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AoC2021
 {
 	partial class Program
 	{
-		// [UseSRL] // Uncomment if you wanna use SuperReadLine
-		// [NoTrailingNewLine] // Uncomment to not include an extra blank line in the input at the end
 		static void Day02(List<string> input)
 		{
-			// My code goes here...
+			long x = 0;
+			long y = 0;
+			long y2 = 0;
+			long aim = 0;
+			foreach (string line in input)
+			{
+				var cmd = line.Split(' ');
+				switch (cmd[0])
+				{
+					case "forward":
+						x += int.Parse(cmd[1]);
+						y2 += aim * int.Parse(cmd[1]);
+						break;
+					case "down":
+						y += int.Parse(cmd[1]);
+						aim += int.Parse(cmd[1]);
+						break;
+					case "up":
+						y -= int.Parse(cmd[1]);
+						aim -= int.Parse(cmd[1]);
+						break;
+					default:
+						break;
+				}
+			}
+			Console.WriteLine($"Part 1: {x * y}\nPart 2: {x * y2}");
 		}
 	}
 }
